5d5dda16fae5f489814dacb278dae016213a46b3
sumansbg025 <sumansbg25@outlook.com>
2021-01-22 09:12:08 +0530

    Paytm Integration application

---
+++ SB-Paytm-Payment/src/main/java/com/pay/app/SbPaytmPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.pay.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbPaytmPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbPaytmPaymentApplication.class, args);
+	}
+
+}
---
+++ SB-Paytm-Payment/src/test/java/com/pay/app/SbPaytmPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pay.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbPaytmPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
